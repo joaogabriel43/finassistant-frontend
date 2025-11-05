31b377e96acd3dbf621cbd8c67294f7b28c0cb27
João Gabriel Borba do Nascimento <108697751+joaogabriel43@users.noreply.github.com>
2025-11-05 11:22:32 -0300

    IntegraçãoNgrok(nao funcional ainda)

--- vite.config.js
+++ vite.config.js
@@ -22,6 +22,7 @@ export default defineConfig(({ mode }) => {
           changeOrigin: true,
           secure: false,
           headers,
+          // rewrite: (path) => path.replace(/^\/api/, ''),
         }
       }
     }
